f6a8185afdef2ad1d8e16ffa719d16477af06f4f
whoanni <68315289+wwwhoanni@users.noreply.github.com>
2026-09-06 06:42:59 -0300

    Actualizacion Script; Comienzo CSS
    Funcion azar terminada, el usuario ya puede dejar dados en la mesa y tirar los otros;
    Falta ver la cuadricula, cargar imagenes y desarrollar practicamente todo el CSS.

--- dados/scriptDados.js
+++ dados/scriptDados.js
@@ -14,8 +14,8 @@ const dado = [
     varHongo
                 ];
 //estos seran os valhores que o usuario vaya eligiendo
-let seleccion = [];
-
+let preseleccion = [];
+let numeroTiradas = 0;
 
 //aqui "tirada" es el nombre del boton y al accionarlo se ejecuta la funcion
 const tirada = document.getElementById("tirada");
@@ -23,22 +23,117 @@ const tirada = document.getElementById("tirada");
 tirada.addEventListener ("click", () => {
     console.log("El usuario tiró los dados");
 azar (dado,limite); 
-console.log(seleccion)
+console.log(preseleccion);
+numeroTiradas++
+saltoLinea (contadorDadosPermaMesa, limite, numeroTiradas)
 });
 
-// la funcion azar deberia devolver un objeto con 5 valores random del dado:
-// si para la tirada dos y tres el usuario selecciona dados que no tira deberiamos contarlos y restarselos al limite
-// declaracion IA : pregunte como tomar un valor random de un objeto y me recomendo object.value; no recuerdo haber usado eso en la cursada; si copie el codigo math random math floor para tomar un valor aleatorio dentro de las posibilidades del dado (:)
 let limite = 5;
 let contadorDadosPermaMesa = 0;
+let seleccion = [null,null,null,null,null];
 
 function azar (dado,limite)  {
-limite = limite - contadorDadosPermaMesa;
-    let valores = [];
-    //recorro dado? ya conozco el nombre de las varaibles
-    for (i=0; i<limite; i++) {
+    for (let i=0; i<limite; i++) {
+        if (seleccion[i]==null){
       let valorAleatorio = dado[Math.floor(Math.random() * dado.length)];  
-     valores.push (valorAleatorio)
-    }
-   seleccion = valores;
+     preseleccion [i] = valorAleatorio;
+    }}
 } 
+
+
+//usuario eligiendo que dados quedan en la mesa:
+const dado1 = document.getElementById("dado1");
+const dado2 = document.getElementById("dado2");
+const dado3 = document.getElementById("dado3");
+const dado4 = document.getElementById("dado4");
+const dado5 = document.getElementById("dado5");
+
+// "encendido-apagado" de las selecciones
+let encendido1 = false;
+let encendido2 = false;
+let encendido3 = false;
+let encendido4 = false;
+let encendido5 = false;
+
+// sea una buena idea armar un for? para cargar un array que vaya actualizando os valhoras das lineas!
+let cuadricula = [];
+
+    // cuadricula (1) = linea [1],linea [2],linea [3],linea [4],linea [5]; actualiza (...)
+    // cuadricula (2) = linea [1],linea [2],linea [3],linea [4],linea [5]; actualiza (...)
+    // cuadricula guardariaa los valores de cada tirada!
+
+let linea = [];
+//declaracion IA: GPT me ayudo a revisar el if : habia olvidado descontar el contador al des-seleccionar <:)
+
+dado1.addEventListener ("click", () => {
+    if (encendido1 === false) { 
+    console.log("El usuario seleccionó dado1");
+    contadorDadosPermaMesa ++;
+    seleccion [0] = preseleccion [0];
+        encendido1 = true 
+    } else {encendido1 = false;
+        contadorDadosPermaMesa--;
+    }
+});
+
+dado2.addEventListener ("click", () => {
+     if (encendido2 === false) {
+    console.log("El usuario seleccionó dado2");
+    contadorDadosPermaMesa ++;
+    seleccion [1] = preseleccion [1];}
+     else {encendido2 = false;
+        contadorDadosPermaMesa--;
+     }
+});
+
+dado3.addEventListener ("click", () => {
+     if (encendido3 === false) {
+    console.log("El usuario seleccionó dado3");
+    contadorDadosPermaMesa ++;
+    seleccion [2] = preseleccion [2];}
+     else {encendido3 = false;
+        contadorDadosPermaMesa--;
+     }
+});
+
+dado4.addEventListener ("click", () => {
+     if (encendido4 === false) {
+        console.log("El usuario seleccionó dado4");
+    contadorDadosPermaMesa ++;
+    seleccion [3] = preseleccion [3];}
+     else {encendido4 = false;
+        contadorDadosPermaMesa--
+     }
+});
+
+dado5.addEventListener ("click", () => {
+     if (encendido5 === false) {
+    console.log("El usuario seleccionó dado5");
+    contadorDadosPermaMesa ++;
+    seleccion [4] = preseleccion [4];}
+     else {encendido5 = false;
+        contadorDadosPermaMesa--;
+     }
+});
+
+// n sera la posicion de la linea
+let n=0;
+function saltoLinea (){
+if (contadorDadosPermaMesa === limite || numeroTiradas === 3) {
+   //reseteo botones, contador y actualizo cuadricula
+   numeroTiradas = 0;
+   contadorDadosPermaMesa = 0;
+
+   linea [n] = seleccion;
+
+   cuadricula.push (linea);
+
+encendido1 = false;
+encendido2 = false;
+encendido3 = false;
+encendido4 = false;
+encendido5 = false;
+
+console.log(linea);
+n++
+} }
